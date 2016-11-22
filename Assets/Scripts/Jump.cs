dd573896c8342da1302a9796bcfaded1ab871afa
sebastiaan <sebastiaanvoebal@hotmail.com>
2016-11-22 15:07:27 +0100

    jump

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Jump : MonoBehaviour
+{
+    [SerializeField]
+    private bool grounded = true;
+    private float jumpPower = 190f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+  
+
+    void Update()
+    {
+            if (grounded && Input.GetButtonDown("Jump"))
+            {
+                rb.AddForce(transform.up * jumpPower);
+                grounded = false;
+            }            
+            else if (!grounded && rb.velocity.y == 0)
+            {
+                grounded = true;
+            }
+    }
+}
