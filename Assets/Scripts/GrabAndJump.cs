a3ca307bc1c38f346e0a891df48e5aa140b41bf1
jvdwijk <joeyvdwijk@gmail.com>
2016-11-22 15:13:05 +0100

    grab and jump script start and level start

---
+++ Assets/Scripts/GrabAndJump.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabAndJump : MonoBehaviour {
+
+	private bool hanging = false;
+
+	void Start () {
+	
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if(other.tag == "rocks")
+		{
+			hanging = true;
+			if(Input.GetKeyUp (KeyCode.Space) )
+			{
+
+			}
+		}
+	}
+
+	void OnKeyUp()
+	{
+		
+	}
+}
