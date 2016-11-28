e2073ad5894462ba6c049741a412b92ea002c922
jvdwijk <joeyvdwijk@gmail.com>
2016-11-28 14:33:37 +0100

    ui bars update

---
+++ Assets/Scripts/BarFiller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BarFiller : MonoBehaviour {
+	[SerializeField]
+	private Image staminaBar;
+
+	void Start () {
+		staminaBar.fillAmount = 0.5f;
+		InvokeRepeating ("IncreaseBar", 2, 2);
+	}
+
+	private void IncreaseBar()
+	{
+		Debug.Log ("asfhiuog");
+		if(staminaBar.fillAmount < 1)
+			staminaBar.fillAmount += 1/6f;
+	}
+
+}
