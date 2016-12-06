f811c1a3ca0dba46c4d418a505f9168b30ad1b36
jvdwijk <joeyvdwijk@gmail.com>
2016-12-06 11:19:55 +0100

    idle animation and other animations update

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -22,7 +22,6 @@ public class PlayerHealth : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("hoer");
         if (other.transform.tag == "sukkel")
         {
             
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,22 +5,29 @@ public class Movement : MonoBehaviour {
     [SerializeField]
     private int speed = 5;
     private Rigidbody2D rb;
+	private Animator anim;
     
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
-
+		anim = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
     void FixedUpdate () {
+		if(!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+		{
+			anim.SetBool ("isRunning", false);
+		}
         if (Input.GetKey(KeyCode.D))
         {
+			anim.SetBool ("isRunning", true);
             transform.Translate(speed * Time.deltaTime, 0, 0);
         }
         if (Input.GetKey(KeyCode.A))
         {
+			anim.SetBool ("isRunning", true);
             transform.Translate(-speed * Time.deltaTime, 0, 0);
         }
     }
