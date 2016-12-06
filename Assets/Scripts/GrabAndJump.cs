45ad9cdc866c8a16287a8a4f29aa94ee27aeed65
Jelmer Le Mair <jelmerlm1@gmail.com>
2016-12-06 14:37:29 +0100

    animatie  stuff in de grab n jump

--- Assets/Scripts/GrabAndJump.cs
+++ Assets/Scripts/GrabAndJump.cs
@@ -5,23 +5,29 @@ public class GrabAndJump : Jump {
 
 	private bool hanging = false;
     private Movement movement;
+    private Animator anim;
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         movement = GetComponent<Movement>();
+        jumpPower = 0;
     }        
 
     void Update()
     {
         if (hanging)
         {
+            anim.SetBool("isSwinging", true);
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 Jumped();
                 hanging = false;
-                rb.gravityScale = .20f;
+                rb.gravityScale = .5f;
 				movement.enabled = true;
+                anim.SetBool("isSwinging", false);
+                anim.SetBool("isJumping", true);
             }
         }
     }
