19e727e1cdc79469ba098eaf7a9ad7e251f5b0ba
jvdwijk <joeyvdwijk@gmail.com>
2016-12-12 20:55:11 +0100

    end scene button functions
    exitgame en SceneChanger gemaakt

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+    public void ChangeScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
