8732d1e0c045eeb3222e02889f4b856e82c63bd7
jvdwijk <joeyvdwijk@gmail.com>
2016-12-12 15:51:10 +0100

    Turn Around anim

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -4,6 +4,7 @@ using System.Collections;
 
 public class Jump : MonoBehaviour
 { 
+	[SerializeField]
 	protected bool grounded = true;
 	[SerializeField]
 	protected float jumpPower = 1750f;
@@ -13,6 +14,7 @@ public class Jump : MonoBehaviour
     private Vector2 playerCurrentPosition;
     private Vector3 startCast;
     private Vector3 endCast;
+	[SerializeField]
 	private RaycastHit2D simbaJump;
 
     void Start()
@@ -32,28 +34,31 @@ public class Jump : MonoBehaviour
         playerCurrentPosition = rb.transform.position;
         startCast = playerCurrentPosition;
         endCast = startCast;
-        startCast.y -= 0.85f;
-        endCast.y -= 0.95f;
+        startCast.y -= 0.65f;
+        endCast.y -= 0.75f;
         if (grounded == false)
         {
              
             simbaJump = Physics2D.Linecast(startCast, endCast);
 			Debug.DrawLine (startCast, endCast);
-            if (simbaJump.collider.tag == "Floor")
-            {
-                grounded = true;
-                anim.SetBool("isJumping", false);
-            }
+			if (simbaJump.collider != null) {
+				if (simbaJump.collider.tag == "Floor") {
+					grounded = true;
+					anim.SetBool ("isJumping", false);
+				}
+			}
+			else if (!grounded && rb.velocity.y == 0)
+			{
+				grounded = true;
+				anim.SetBool ("isJumping", false);
+			}
         }
         else if (grounded && Input.GetButtonDown("Jump"))
         {
 			anim.SetBool ("isJumping", true);
 			Jumped ();
          }            
-         /*  else if (!grounded && rb.velocity.y == 0)
-         {
-            grounded = true;
-         }*/
+         /*  */
     }
 
 	protected void Jumped()
