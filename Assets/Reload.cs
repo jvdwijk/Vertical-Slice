527b8f766b0b372e2a92ff37b0cacecc0f383dd6
jvdwijk <joeyvdwijk@gmail.com>
2016-12-12 13:53:30 +0100

    reload scene

---
+++ Assets/Reload.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Reload : MonoBehaviour {
+	[SerializeField]
+	private int scene;
+
+	void Update () 
+	{
+		if(Input.GetKeyUp (KeyCode.R))
+		{
+			ChangeScene ();
+		}
+	}
+
+	private void ChangeScene()
+	{
+		SceneManager.LoadScene(scene);
+	}
+
+}
