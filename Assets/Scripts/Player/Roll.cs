a9c696461f89835fa23664c34a3bed449f0d8147
jvdwijk <joeyvdwijk@gmail.com>
2016-12-13 21:48:13 +0100

    spider anim & roll

---
+++ Assets/Scripts/Player/Roll.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Roll : MonoBehaviour {
+
+	private Animator anim;
+	private Movement movement;
+	private GrabAndJump grab;
+	private int direction;
+	private int count = 0;
+
+	void Start () {
+		movement = GetComponent<Movement> ();
+		grab = GetComponent<GrabAndJump> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update () {
+		if(Input.GetKey (KeyCode.A) && Input.GetKeyDown (KeyCode.S) && !grab.hanging)
+		{
+			direction = -1;
+			RollAway ();
+		}
+		else if(Input.GetKey (KeyCode.D) && Input.GetKeyDown (KeyCode.S) && !grab.hanging)
+		{
+			direction = 1;
+			RollAway ();
+		}
+	}
+
+	private void RollAway()
+	{
+		anim.SetTrigger ("rolled");
+		movement.enabled = false;
+		StartCoroutine (RollTime ());
+	}
+
+	private IEnumerator RollTime()
+	{
+		while(count < 12)
+		{
+			transform.Translate(10 * direction * Time.deltaTime,0,0);
+			yield return new WaitForSeconds (0.05f);
+			count++;
+		}
+		count = 0;
+		ResetRoll ();
+	}
+
+	private void ResetRoll()
+	{
+		movement.enabled = true;
+	}
+}
