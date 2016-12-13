4194a7752f7bbc140453e7e442494d6d53eada86
jvdwijk <joeyvdwijk@gmail.com>
2016-12-13 15:33:09 +0100

    run animatie & targetfollow

---
+++ Assets/Scripts/TargetFollow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class TargetFollow : MonoBehaviour
+{
+	public Transform target;
+	public Vector3 offset = new Vector3(0f, 7.5f, 0f);
+
+	private void LateUpdate()
+	{
+		transform.position = target.position + offset;
+	}
+}
