6e3bc9c5eec53f31e33241976fca6b608b8310d7
jvdwijk <joeyvdwijk@gmail.com>
2016-12-15 12:13:28 +0100

    swing animatie update & aap update

---
+++ Assets/Scripts/SceneManagement/EndSceneAnimations.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndSceneAnimations : MonoBehaviour {
+
+	private Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+		Invoke ("SetTrue", 1);
+	}
+
+	private void SetTrue()
+	{
+		anim.SetBool ("doneFirst", true);
+	}
+
+	public void Trigger()
+	{
+		anim.SetTrigger ("yes");
+	}
+}
