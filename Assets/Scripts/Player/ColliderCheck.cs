fbeb2e7662a60640c9715944a0363029e638380e
jvdwijk <joeyvdwijk@gmail.com>
2016-12-15 12:37:28 +0100

    small tweaks

--- Assets/Scripts/Player/ColliderCheck.cs
+++ Assets/Scripts/Player/ColliderCheck.cs
@@ -4,15 +4,29 @@ using System.Collections;
 public class ColliderCheck : MonoBehaviour {
 
 	public bool hanging;
+	private Animator anim;
+
+	void Start()
+	{
+		anim = GetComponent<Animator> ();
+	}
 
 	void OnTriggerEnter2D (Collider2D other) 	
 	{
 		if(other.gameObject.CompareTag ("rocks"))
 		{
-			Debug.Log ("test");
 			hanging = true;
 		}
 	}
+
+	void OnColliderEnter2D (Collision2D other) 	
+	{
+		if(other.gameObject.CompareTag ("slide"))
+		{
+			anim.SetTrigger ("slide");
+		}
+	}
+
 	void OnTriggerExit2D (Collider2D other) 
 	{
 		if(other.gameObject.CompareTag ("rocks"))
---
+++ Assets/Scripts/Player/IgnoreLayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnoreLayer : MonoBehaviour {
+
+	[SerializeField]
+	private int LayerOne;
+	[SerializeField]
+	private int LayerTwo;
+
+	void Start () {
+		Physics2D.IgnoreLayerCollision(LayerOne, LayerTwo);
+	}
+}
