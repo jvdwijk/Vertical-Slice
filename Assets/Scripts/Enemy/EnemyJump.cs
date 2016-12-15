e30b69049fc5d460fb1c05d90d23fc554efb1606
Jelmer Le Mair <jelmerlm1@gmail.com>
2016-12-15 17:12:14 +0100

    enemy random jump

---
+++ Assets/Scripts/Enemy/EnemyJump.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyJump : MonoBehaviour {
+
+    [SerializeField]
+    private float jumpPower = 500;
+    private Rigidbody2D rb;
+    private bool visible = true;
+
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(JumpTime());
+	}
+	
+	void Jump () {
+        rb.AddForce(transform.up * jumpPower);
+        Debug.Log("whooped");
+    }
+
+    void OnBecameInvisible()
+    {
+        DestroyObject(this.gameObject);
+        visible = false;    
+    }    
+
+        
+
+
+    IEnumerator JumpTime()
+    {
+        float minWaitTime = 2;
+        float maxWaitTime = 5;
+
+        while (visible == true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+            Jump();
+
+        }
+
+    }
+}
