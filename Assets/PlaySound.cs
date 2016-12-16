bf621e3f10d3aedfdedb48af302acd7521e6460b
jvdwijk <joeyvdwijk@gmail.com>
2016-12-16 11:10:37 +0100

    geluid update

---
+++ Assets/PlaySound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour {
+
+	private bool playSound = false;
+	[SerializeField]
+	private AudioSource ratSound;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		if (playSound == true) {
+			Debug.Log ("afds");
+			ratSound.Play ();
+		}
+	}
+
+	void OnBecameVisible()
+	{
+		playSound = true;
+	}
+}
